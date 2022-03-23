63a0ac61ae6fc4ec1353dd4668d67501b73fdbe6
hillario <hillches@gmail.com>
2022-03-24 01:25:41 +0300

    Updates

---
+++ precache-manifest.eeab8933cc341f0e1a02aad303ebe419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d168ffc08a655122b06be393e041a4fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ada96abd7173227bdcf",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6ada96abd7173227bdcf",
+    "url": "/static/js/2.74abc827.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.74abc827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18baa9c9ecbabb821344",
+    "url": "/static/js/main.10d6ae49.chunk.js"
+  },
+  {
+    "revision": "1d05d21cfb7c11100d5b",
+    "url": "/static/js/runtime-main.5be0474a.js"
+  }
+]);
